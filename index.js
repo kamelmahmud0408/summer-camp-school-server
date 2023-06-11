6000eacdbfc5524d49a0de3866e43b486eccd57e
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-11 20:17:57 +0600

    check user role code

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
-const app= express()
+const app = express()
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'Unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 
 
@@ -27,13 +45,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
-    const usersCollection= client.db('sportsDb').collection('users')
-    const classesCollection= client.db('sportsDb').collection('classes')
-    const enrolledCollection= client.db('sportsDb').collection('enrolled')
 
-   // JWT
-    
+    const usersCollection = client.db('sportsDb').collection('users')
+    const classesCollection = client.db('sportsDb').collection('classes')
+    const selectedCollection = client.db('sportsDb').collection('selected')
+
+    // JWT
+
     app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -44,67 +62,85 @@ async function run() {
 
 
 
-  // users
+    // users
 
-  app.get('/users',  async (req, res) => {
-    const result = await usersCollection.find().toArray()
-    res.send(result)
-  })
-
-
-  app.post('/users', async (req, res) => {
-    const user = req.body;
-    const query = { email: user.email }
-    const existingUser = await usersCollection.findOne(query)
-    if (existingUser) {
-      return res.send({ message: 'User already exist' })
-    }
-    const result = await usersCollection.insertOne(user)
-    res.send(result)
-  })
-
-  app.patch('/users/admin/:id', async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) }
-    const updateDoc = {
-      $set: {
-        role: 'admin'
-      },
-    };
-    const result = await usersCollection.updateOne(filter, updateDoc)
-    res.send(result)
-  })
-    
-  // classes
-    app.get('/classes', async(req,res)=>{
-        
-        const result= await classesCollection.find().sort({number_of_students: -1}).toArray()
-        res.send(result)
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
     })
 
-    // enrolledcollection
 
-    app.get('/enrolled', async(req,res)=>{
-      const email= req.query.email;
-      
-      if(!email){
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'User already exist' })
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    // classes
+    app.get('/classes', async (req, res) => {
+
+      const result = await classesCollection.find().sort({ number_of_students: -1 }).toArray()
+      res.send(result)
+    })
+
+    // selectedcollection
+
+    app.get('/selected', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
         res.send([])
       }
-      const query = {email: email};
-      const result=await enrolledCollection.find(query).toArray()
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+      else {
+        const query = { email: email };
+        const result = await selectedCollection.find(query).toArray()
+        res.send(result)
+      }
+    })
+
+    app.post('/selected', async (req, res) => {
+      const item = req.body;
+      const result = await selectedCollection.insertOne(item)
       res.send(result)
     })
 
-    app.post('/enrolled', async(req,res)=>{
-      const item =req.body;
-      const result= await enrolledCollection.insertOne(item)
-      res.send(result)
-    })
-
-    app.delete('/enrolleddelete/:id', async (req, res) => {
+    app.delete('/selectedDelete/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
-      const result = await enrolledCollection.deleteOne(query)
+      const result = await selectedCollection.deleteOne(query)
       res.send(result)
     })
 
@@ -123,9 +159,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('summer school is running....')
-  })
-  
-  app.listen(port, () => {
-    console.log(`summer school is running on port ${port}`)
-  })
+  res.send('summer school is running....')
+})
+
+app.listen(port, () => {
+  console.log(`summer school is running on port ${port}`)
+})
